3590edc0fcbeabe9e553420eb06f93fe257aaca4
Nicolas Brassard <nicolasbrassard10401@gmail.com>
2023-10-25 22:22:20 -0700

    more work

--- crates/zig/src/lib.rs
+++ crates/zig/src/lib.rs
@@ -1,5 +1,9 @@
+use std::fmt::Write;
+
 use wit_bindgen_core::{
-    wit_parser::{Function, InterfaceId, Resolve, TypeId, WorldId, WorldKey},
+    abi::{AbiVariant, Bindgen, Instruction, LiftLower},
+    uwrite,
+    wit_parser::{Function, InterfaceId, Resolve, SizeAlign, TypeId, WorldId, WorldKey},
     Files, Source, WorldGenerator,
 };
 
@@ -35,52 +39,134 @@ impl WorldGenerator for Zig {
             .push_str(&format!("// Import functions from {name_raw}\n"));
 
         for (name, func) in resolve.interfaces[iface].functions.iter() {
-            dbg!(name, func);
+            let mut func_bindgen = FunctionBindgen::new(resolve);
+            for (param_name, _param_type) in func.params.iter() {
+                func_bindgen.params.push(param_name.clone());
+            }
+            wit_bindgen_core::abi::call(
+                resolve,
+                AbiVariant::GuestImport,
+                LiftLower::LowerArgsLiftResults,
+                func,
+                &mut func_bindgen,
+            );
+            self.src.push_str(&func_bindgen.src);
         }
-        todo!()
+
+        files.push(&format!("{name_raw}.zig"), self.src.as_bytes());
     }
 
     fn export_interface(
         &mut self,
-        resolve: &Resolve,
-        name: &WorldKey,
-        iface: InterfaceId,
-        files: &mut Files,
+        _resolve: &Resolve,
+        _name: &WorldKey,
+        _iface: InterfaceId,
+        _files: &mut Files,
     ) -> anyhow::Result<()> {
         todo!()
     }
 
     fn import_funcs(
         &mut self,
-        resolve: &Resolve,
-        world: WorldId,
-        funcs: &[(&str, &Function)],
-        files: &mut Files,
+        _resolve: &Resolve,
+        _world: WorldId,
+        _funcs: &[(&str, &Function)],
+        _files: &mut Files,
     ) {
         todo!()
     }
 
     fn export_funcs(
         &mut self,
-        resolve: &Resolve,
-        world: WorldId,
-        funcs: &[(&str, &Function)],
-        files: &mut Files,
+        _resolve: &Resolve,
+        _world: WorldId,
+        _funcs: &[(&str, &Function)],
+        _files: &mut Files,
     ) -> anyhow::Result<()> {
         todo!()
     }
 
     fn import_types(
         &mut self,
-        resolve: &Resolve,
-        world: WorldId,
-        types: &[(&str, TypeId)],
-        files: &mut Files,
+        _resolve: &Resolve,
+        _world: WorldId,
+        _types: &[(&str, TypeId)],
+        _files: &mut Files,
     ) {
         todo!()
     }
 
-    fn finish(&mut self, resolve: &Resolve, world: WorldId, files: &mut Files) {
+    fn finish(&mut self, _resolve: &Resolve, _world: WorldId, _files: &mut Files) {
+        todo!()
+    }
+}
+
+struct FunctionBindgen {
+    params: Vec<String>,
+    src: Source,
+    sizes: SizeAlign,
+}
+
+impl FunctionBindgen {
+    fn new(resolve: &Resolve) -> FunctionBindgen {
+        let mut sizes = SizeAlign::default();
+        sizes.fill(resolve);
+        FunctionBindgen {
+            params: Vec::new(),
+            src: Source::default(),
+            sizes,
+        }
+    }
+}
+
+impl Bindgen for FunctionBindgen {
+    type Operand = String;
+
+    fn emit(
+        &mut self,
+        resolve: &Resolve,
+        inst: &Instruction<'_>,
+        operands: &mut Vec<Self::Operand>,
+        results: &mut Vec<Self::Operand>,
+    ) {
+        match inst {
+            Instruction::GetArg { nth } => results.push(self.params[*nth].clone()),
+            Instruction::StringLower { realloc } => {
+                let str = &operands[0];
+                results.push(format!("{str}.ptr"));
+                results.push(format!("{str}.len"));
+            }
+            Instruction::CallWasm { name, sig } => {
+                uwrite!(self.src, "");
+            }
+            Instruction::Return { amt, func } => {
+                dbg!(amt, func);
+            }
+            _ => todo!("{inst:?}"),
+        }
+    }
+
+    fn return_pointer(&mut self, _size: usize, _align: usize) -> Self::Operand {
+        todo!()
+    }
+
+    fn push_block(&mut self) {
+        todo!()
+    }
+
+    fn finish_block(&mut self, _operand: &mut Vec<Self::Operand>) {
+        todo!()
+    }
+
+    fn sizes(&self) -> &SizeAlign {
+        &self.sizes
+    }
+
+    fn is_list_canonical(
+        &self,
+        _resolve: &Resolve,
+        _element: &wit_bindgen_core::wit_parser::Type,
+    ) -> bool {
         todo!()
     }
 }
